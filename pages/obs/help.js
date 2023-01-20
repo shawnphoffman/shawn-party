ab4fbfb6e745fc6c2b4401ee2232bbd23c6a5879
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-01-20 15:46:05 -0800

    Text helper

--- pages/obs/help.js
+++ pages/obs/help.js
@@ -1,8 +1,55 @@
-import { memo, useMemo } from 'react'
+import { memo, useCallback, useEffect, useState } from 'react'
 import { styled } from 'linaria/react'
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
+
+const NativeControl = styled.input`
+	margin: 0;
+	margin-bottom: 8px;
+	padding: 12px 16px;
+	/* font-size: 18px; */
+	line-height: 1.2;
+	text-align: start;
+	text-indent: 0px;
+	text-transform: none;
+	word-spacing: 0px;
+	/* border-color: var(--inputBorder); */
+	border-style: solid;
+	border-width: 0;
+	/* background-color: var(--transparent); */
+	opacity: 1;
+	flex: 1;
+	user-select: all;
+	/* color: var(--bg); */
+
+	&:focus {
+		outline-style: none;
+		box-shadow: none;
+		/* border-color: var(--transparent); */
+	}
+
+	&:read-only {
+		color: grey;
+		user-select: all;
+	}
+
+	width: 100%;
+	/* height: 100px; */
+`
 
 const Help = () => {
+	const [original, setOriginal] = useState('')
+	const [encoded, setEncoded] = useState('')
+
+	const handleOriginalChange = useCallback(e => {
+		const value = e.target.value
+		setOriginal(value)
+	}, [])
+
+	useEffect(() => {
+		const temp = encodeURIComponent(original)
+		setEncoded(temp)
+	}, [original])
+
 	return (
 		<>
 			<h1>OBS Source Help</h1>
@@ -38,6 +85,10 @@ const Help = () => {
 					</a>
 				</li>
 			</ul>
+			<h3>Text Encoder</h3>
+			<NativeControl type="text" onChange={handleOriginalChange} value={original} />
+			{/* {encoded && <NativeControl value={`text=${encoded}`} readOnly />} */}
+			{encoded && <div style={{ userSelect: 'all' }}>{`text=${encoded}`}</div>}
 		</>
 	)
 }
