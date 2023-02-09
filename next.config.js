edc8ba6763fa50e5b7d80096c0ae3275c0b3b742
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-02-08 16:08:45 -0800

    Remove OBS stuff

--- next.config.js
+++ next.config.js
@@ -4,6 +4,30 @@ const withLinaria = require('next-linaria')
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
+	async redirects() {
+		return [
+			{
+				source: '/obs/help',
+				destination: 'https://obs.shawn.party',
+				permanent: true,
+			},
+			{
+				source: '/obs/text',
+				destination: 'https://obs.shawn.party/render/text',
+				permanent: true,
+			},
+			{
+				source: '/obs/twitch/lookup',
+				destination: 'https://obs.shawn.party/twitch/lookup',
+				permanent: true,
+			},
+			{
+				source: '/obs/followers/:path*',
+				destination: 'https://obs.shawn.party/render/followers/:path*',
+				permanent: true,
+			},
+		]
+	},
 }
 
 module.exports = withLinaria(nextConfig)
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import 'styles/globals.css'
-import 'styles/obs.css'
 
 import Head from 'next/head'
 
