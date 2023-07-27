94d2e6c61d1fbbf40652423281b8d2d9d9e8b9d8
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-07-27 00:10:42 -0700

    Add comics page

---
+++ app/(pages)/star-wars/comics/page.js
@@ -0,0 +1,41 @@
+import { memo } from 'react'
+
+const dataUrl = 'https://api.shawn.party/api/syno/comics'
+
+async function getData() {
+	try {
+		const res = await fetch(dataUrl, { next: { revalidate: 60 * 60 } })
+		const data = await res.json()
+
+		const { files } = data
+
+		return {
+			files,
+		}
+	} catch {
+		return {}
+	}
+}
+
+const Comics = async () => {
+	const data = await getData()
+
+	const hasFiles = !!data?.files?.length
+
+	return (
+		<>
+			<h1>Star Wars Comics</h1>
+			{hasFiles ? (
+				<ul>
+					{data.files.map(file => (
+						<li key={file}>{file}</li>
+					))}
+				</ul>
+			) : (
+				<p>Unable to load files...</p>
+			)}
+		</>
+	)
+}
+
+export default memo(Comics)
