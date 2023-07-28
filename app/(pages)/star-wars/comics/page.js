a709e2c0926aa0b18aaa4016baa38b3b03879308
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2023-07-28 16:02:45 -0700

    Update comics page

--- app/(pages)/star-wars/comics/page.js
+++ app/(pages)/star-wars/comics/page.js
@@ -2,10 +2,24 @@ import { memo } from 'react'
 
 const dataUrl = 'https://api.shawn.party/api/syno/comics'
 
+const requestOptions = {
+	method: 'POST',
+	next: { revalidate: 60 * 60 * 24 * 3 },
+	body: JSON.stringify({
+		secret: process.env.SYNO_SECRET,
+	}),
+	headers: {
+		Accept: 'application/json',
+		'Content-Type': 'application/json',
+	},
+}
+
 async function getData() {
 	try {
-		const res = await fetch(dataUrl, { next: { revalidate: 60 * 60 * 24 } })
+		const res = await fetch(dataUrl, requestOptions)
 		const data = await res.json()
+
+		console.log('data', data)
 
 		const { files } = data
 
