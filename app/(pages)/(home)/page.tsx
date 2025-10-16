3c9b2ceb9448c77de5e491195dfb3f77974cd6e4
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2025-10-15 19:38:17 -0700

    Make the head ridiculous

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 
 import head from './head.png'
+import ShawnHead from './party'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBluesky, faDiscord, faGithub, faInstagram, faLinkedin } from '@awesome.me/kit-d7ccc5bb1a/icons/classic/brands'
@@ -16,12 +17,15 @@ function Home() {
 	return (
 		<div className="flex flex-col gap-12">
 			<div className={'flex flex-row gap-8 w-full'}>
-				<Image
+				<div className="min-w-24 w-fit sm:max-h-44 md:max-h-60 hidden drop-shadow-big active:drop-shadow-none transition-all active:translate-y-3 active:translate-x-3 active:rotate-10 [&>svg]:w-full xs:flex items-center justify-center overflow-visible">
+					<ShawnHead />
+				</div>
+				{/* <Image
 					className="min-w-0 object-scale-down max-h-32 w-fit sm:max-h-44 md:max-h-60 hidden xs:block drop-shadow-big active:drop-shadow-none transition-all active:translate-y-3 active:translate-x-3 active:rotate-10"
 					width={158}
 					src={head}
 					alt="Illustration of my goofy face"
-				/>
+				/> */}
 				<Suspense fallback={<Title />}>
 					<TitleClient />
 				</Suspense>
